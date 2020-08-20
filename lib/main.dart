7847027db6eaeaf8f96ba95177f762979f9b42ef
Alfredo Rinaudo <alfredo.rinaudo@evertecinc.com>
2020-08-20 09:23:16 -0300

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:beers_pairing/bloc/beers_bloc.dart';
 import 'package:beers_pairing/bloc/provider.dart';
+import 'package:beers_pairing/screens/random_beer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,153 +16,74 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: MainContainer(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class MainContainer extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainContainerState createState() => _MainContainerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void didUpdateWidget(MyHomePage oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    Provider.beersBlocOf(context).getRandomBeer();
-  }
-
+class _MainContainerState extends State<MainContainer> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {
-              Provider.beersBlocOf(context).getRandomBeer();
-            },
-          ),
-        ],
-      ),
-      body: RandomBeerWidget(),
-    );
-  }
-}
-
-class RandomBeerWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<BeersSate>(
-      stream: Provider.beersBlocOf(context).streamBeersSate,
-      initialData: BeersSate(),
-      builder: (BuildContext context, AsyncSnapshot<BeersSate> snapshot) {
-        if (snapshot.data.loadingRandomBeer ||
-            snapshot.data.currentRandomBeer == null) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return SingleChildScrollView(
-          padding: const EdgeInsets.only(bottom: 20.0),
-          child: Container(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  aspectRatio: 1.0,
-                  child: (snapshot.data.currentRandomBeer.imageUrl != null)
-                      ? Image.network(snapshot.data.currentRandomBeer.imageUrl)
-                      : Container(
-                          color: Colors.grey.withOpacity(0.2),
-                        ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 12.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.only(right: 8.0),
-                          child: Text(
-                            snapshot.data.currentRandomBeer.name,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 20.0,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Beers Pairing"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                Provider.beersBlocOf(context).getRandomBeer();
+              },
+            ),
+          ],
+        ),
+        body: OrientationBuilder(
+          builder: (BuildContext context, Orientation orientation) {
+            // print(orientation);
+            return Column(
+              children: <Widget>[
+                (orientation == Orientation.portrait)
+                    ? TabBar(
+                        tabs: [
+                          Tab(
+                            child: Text(
+                              "Random Beer",
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
                             ),
                           ),
-                        ),
+                          Tab(
+                            child: Text(
+                              "Paired Beers",
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Container(width: 0.0, height: 0.0),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      RandomBeerBody(
+                        orientation: orientation,
                       ),
-                      AbvWidget(
-                          percentage: snapshot.data.currentRandomBeer.abv),
+                      Container(),
                     ],
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 12.0, bottom: 12.0),
-                  child: Text(
-                    snapshot.data.currentRandomBeer.description,
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                ),
-                Text(
-                  snapshot.data.currentRandomBeer.tagline,
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.grey,
-                  ),
-                ),
               ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class AbvWidget extends StatelessWidget {
-  const AbvWidget({
-    Key key,
-    @required this.percentage,
-  }) : super(key: key);
-
-  final double percentage;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding:
-          const EdgeInsets.only(left: 8.0, top: 4.0, right: 8.0, bottom: 4.0),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.all(
-          Radius.circular(12.0),
-        ),
-      ),
-      child: Text(
-        "$percentage%",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 12.0,
+            );
+          },
         ),
       ),
     );
---
+++ lib/screens/random_beer.dart
@@ -0,0 +1,139 @@
+import 'package:beers_pairing/bloc/beers_bloc.dart';
+import 'package:beers_pairing/bloc/provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class RandomBeerBody extends StatefulWidget {
+  RandomBeerBody({
+    Key key,
+    this.orientation,
+  });
+  final Orientation orientation;
+
+  @override
+  _RandomBeerBodyState createState() => _RandomBeerBodyState();
+}
+
+class _RandomBeerBodyState extends State<RandomBeerBody> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (Provider.beersBlocOf(context).currentBeersState().currentRandomBeer ==
+        null) {
+      Provider.beersBlocOf(context).getRandomBeer();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(widget.orientation);
+    return StreamBuilder<BeersSate>(
+      stream: Provider.beersBlocOf(context).streamBeersSate,
+      builder: (BuildContext context, AsyncSnapshot<BeersSate> snapshot) {
+        if (!snapshot.hasData || snapshot.data.loadingRandomBeer) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return SingleChildScrollView(
+          padding: const EdgeInsets.only(bottom: 20.0),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  child: AspectRatio(
+                    aspectRatio: 1.0,
+                    child: CachedNetworkImage(
+                      imageUrl: snapshot.data.currentRandomBeer.imageUrl ??
+                          "https://via.placeholder.com/350?text=No+Image",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.only(top: 12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 8.0),
+                                child: Text(
+                                  snapshot.data.currentRandomBeer.name,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            AbvWidget(
+                                percentage:
+                                    snapshot.data.currentRandomBeer.abv),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+                        child: Text(
+                          snapshot.data.currentRandomBeer.description,
+                          style: TextStyle(
+                            fontSize: 15.0,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        snapshot.data.currentRandomBeer.tagline,
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class AbvWidget extends StatelessWidget {
+  const AbvWidget({
+    Key key,
+    @required this.percentage,
+  }) : super(key: key);
+
+  final double percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding:
+          const EdgeInsets.only(left: 8.0, top: 4.0, right: 8.0, bottom: 4.0),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12.0),
+        ),
+      ),
+      child: Text(
+        "$percentage%",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 12.0,
+        ),
+      ),
+    );
+  }
+}
